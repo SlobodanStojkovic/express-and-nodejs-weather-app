5c8c1074a92697de58f0919ef275dfbf4ae5084d
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-13 13:22:22 +0200

    Add Query string for search from address bar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const { response } = require("express");
 
 const app = express();
 
@@ -40,9 +41,29 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
   res.send({
     forecast: "It is sunny",
     location: "Belgrade",
+    address: req.query.address,
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
   });
 });
 
